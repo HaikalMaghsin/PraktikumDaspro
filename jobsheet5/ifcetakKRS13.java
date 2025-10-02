6f4034faf33e8fbfc0a8345f2febaf253e52880a
Haikal Maghsin <haikal.maghsin0756@gmail.com>
2025-10-02 07:41:42 +0700

    Percobaan 1

---
+++ jobsheet5/ifcetakKRS13.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ifcetakKRS13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("--- Cetak KRS SIAKAD ---");
+        System.out.println("Apakah UKT sudah dibayar? (true/false):");
+        boolean uktDibayar = sc.nextBoolean();
+        sc.close();
+
+        if (uktDibayar) {
+            System.out.println("pembayaran UKT sudah lunas.");
+            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA.");
+        }
+    }
+}
